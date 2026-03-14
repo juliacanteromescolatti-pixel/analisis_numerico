a73c664cfdc3ceb0558fd1a9866738cbe7788dfd
Juli Cantero <juliacanteromescolatti@gmail.com>
2026-03-14 12:47:52 -0300

    update 3

--- lab_n1.py
+++ lab_n1.py
@@ -1,11 +1,23 @@
 # Lab 1
-# 1
-x=1
-y=2
-z=3
-print("Ejercicio 1.a")
-print(x/y+z)
-print(x/(y+z))
-print("Ejercicio 1.b")
-print(x/y*z)
-print(x/(y*z))
+#act 1
+# x=1
+# y=2
+# z=3
+# print("Ejercicio 1.a")
+# print(x/y+z)
+# print(x/(y+z))
+# print("Ejercicio 1.b")
+# print(x/y*z)
+# print(x/(y*z))
+def op1(x,y,z):
+    u=x/y+z
+    return u
+def op2(x,y,z):
+    w=x/(y+z)
+    return w
+def op3(x,y,z):
+    h=x/y*z
+    return h
+def op4(x,y,z):
+    v=x/(y*z)
+    return v
