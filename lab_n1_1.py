a1ce6097cbb734ced23cec2c52af513d7c87221e
Juli Cantero <juliacanteromescolatti@gmail.com>
2026-03-16 17:07:25 -0300

    .

---
+++ lab_n1_1.py
@@ -0,0 +1,13 @@
+# Ej1
+def op1(x,y,z):
+    u=x/y+z
+    return u
+def op2(x,y,z):
+    w=x/(y+z)
+    return w
+def op3(x,y,z):
+    h=x/y*z
+    return h
+def op4(x,y,z):
+    v=x/(y*z)
+    return v
