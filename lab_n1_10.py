51b01eb8425e0d94a5ffdba340608d8f1b608f35
Juli Cantero <juliacanteromescolatti@gmail.com>
2026-03-23 13:30:19 -0300

    update8

--- lab_n1_10.py
+++ lab_n1_10.py
@@ -12,3 +12,7 @@ for _ in range(100):
     y = 1 / x
     if not sonreciprocos(x,y):
         print(x)
+
+#vemos que x*y si son iguala uno es True, en caso contrario es False.
+#Luego toamoas 100 nros, random y definimos x como estos nros. e y la divison y pedimos que si no son reciprocos nos imprima solo x
+#EN RESUMEN: genera 100 números aleatorios x entre 1 y 2, luego calcula y = 1/x. Comprueba si son recíprocos con sonreciprocos, luego imprime x si la comprobación falla. Y por ultimo el posible "fallo" se debe a errores de precisión de los números decimales.
