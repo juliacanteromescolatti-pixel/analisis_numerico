4bf424baf3f93f41137ec67734361ee6bfdb4874
Juli Cantero <juliacanteromescolatti@gmail.com>
2026-03-23 13:24:39 -0300

    update7

--- lab_n1_9.py
+++ lab_n1_9.py
@@ -18,3 +18,7 @@ def horn_mal(coef, p):
 
 print(horn(coef,p))
 print(horn_mal(coef,p))
+#lo que  hacemos en este ejercicio es , dados los coeficientes de nuestro polinomios, tomameos como res 
+#Al resultado de haver p por el coef cero( que es el de mas valor)mas el sigient (todo esto gracias a enterder Hornes)
+#lUego notmaos que podemos ahorarnos dps operaciones por eso es i+2, pues la primera esta fija y por Horner nos ahorramos la otra
+#Luego comprobamos que el resultado que nos da el es correcto haciendo en la caluladora
