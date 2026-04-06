1e55b902c6557f0c8a08922e4a0fb5eee6746460
Juli Cantero <juliacanteromescolatti@gmail.com>
2026-04-06 15:54:46 -0300

    update

---
+++ guia_1/P1_ej8_bien.py
@@ -0,0 +1,28 @@
+import math
+def mala(a,b,c):
+    disc=b**2-4*a*c
+
+    if disc<0:
+        print("EL dsicriminante es menor que 0, intente de nuevo")
+        return None
+    
+    x_1=(-b+sqrt(disc))/(2.0*a)
+    x_2=(-b-sqrt(disc))/(2.0*a)
+
+    return[x_1,x_2]
+
+def buena(a,b,c):
+    disc=b**2-4*a*c
+
+    if disc<0:
+        print("EL dsicriminante es menor que 0, intente de nuevo")
+        return None
+
+#Encontrar la raiz mas lejana al cero en valor absoluto
+if b>0:
+    x_1=(-b-sqrt(disc))/(2.0*a)
+else:
+    x_1=(-b+sqrt(disc))/(2.0*a)
+x_2=(c/a)/x_1
+    
+return[x_1,x_2]
