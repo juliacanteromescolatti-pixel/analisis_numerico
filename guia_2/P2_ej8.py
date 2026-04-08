0204dea1f1356950e906d35fd9e100bb04814b66
Juli Cantero <juliacanteromescolatti@gmail.com>
2026-04-08 19:53:13 -0300

    update

--- guia_2/P2_ej8.py
+++ guia_2/P2_ej8.py
@@ -1,3 +1,2 @@
 #act 8
-
-print("sos un intruso te gano encima incluso uso de ir como un buzo buceando ando buscando los recursos retomoel impulso ehhhh cambio el cursor cuando las frases ")
+#FALTA HACER
