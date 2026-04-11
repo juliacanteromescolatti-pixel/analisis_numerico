3caf257b9949ac7859632f646df1e1e20082e407
Juli Cantero <juliacanteromescolatti@gmail.com>
2026-04-11 17:47:09 -0300

    update

--- guia_2/P2_ej8.py
+++ guia_2/P2_ej8.py
@@ -1,2 +1,4 @@
 #act 8
-#FALTA HACER
+"""Calularemos la primera derivada para ver onde esta el minimo, y luego la segunda pues 
+es la que necesitaremos al meomento de aplicar el metodo de Newton.
+Porque para encontrar las raices de la primera, necesito la segunda derivada  asi aplicar el metodo."""
---
+++ guia_2/P2_ej9.py
@@ -0,0 +1,3 @@
+"""Dada la ecuacion, lo que haremso sera restar E de ambos lados, y asi quedar una ecuacion con laincognita D
+(cuadratica) igualada a cero, para asi aplicar el metodo de newton.
+Pues los datos de E y V los tenemos, solo nos falta calcular D"""
