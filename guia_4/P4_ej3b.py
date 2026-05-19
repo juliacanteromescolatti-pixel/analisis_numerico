980ebb20c571273b322404bc994d48446b7b0faf
Juli Cantero <juliacanteromescolatti@gmail.com>
2026-05-19 18:50:27 -0300

    update

--- guia_4/P4_ej3b.py
+++ guia_4/P4_ej3b.py
@@ -3,29 +3,33 @@ el ajuste de los siguientes modelos, es decir, determinar los coeficientes de ca
  (b) y(x) = x/Ax + B
 AYUDA
 Transformar en cada caso la expresión dada a un modelo lineal, y obtener una recta que
-mejor ajusta los datos (transformados) en el sentido de m´ınimos cuadrados.
+mejor ajusta los datos (transformados) en el sentido de mìnimos cuadrados.
 """
 import numpy as np
 import matplotlib.pyplot as plt
 
-datos = np.loadtxt("/home/visitante/Documentos/vj/guia_4/datos3a.dat")
-x = datos[0]
-y = datos[1]
+import numpy as np
+import matplotlib.pyplot as plt
 
-x_ = 1/x
-y_ = 1/y
+# Leer datos
+x, y = np.loadtxt("/home/kmom/analisis_numerico/guia_4/archivos necesarios/datos3b.dat")
+# Transformación lineal
+y_ = x / y
 
-coef = np.polyfit(x_, y_, 1) # porque es de grado uno (lineal)
+# Ajuste lineal
+coef = np.polyfit(x, y_, 1)
 
-B = coef[0] # termino lineal
-A = np.exp(coef[1]) # termino indeprendiente
-print("El valor de A es: ", A)
-print("El valor de B es: ", B)
+A = coef[0] #termino lineal
+B = coef[1] #termono independiente
+
+print("El valor de A es:", A)
+print("El valor de B es:", B)
+
 
 # Graficar
 plt.plot(x,y,"o", color = "red", label="valores de los datos")
 x_plot = np.linspace(1,5,100) # pido x para graficar
-y_plot = (x/A*x_plot+B)
+y_plot = x_plot / (A*x_plot + B)
 plt.plot(x_plot, y_plot)
 plt.legend()
 plt.title("Act 3-(b)")
